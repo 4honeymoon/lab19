e2f556f3a1a030df4e9f48ab94ecbf8835a9ce9e
4honeymoon <mkhutinaeva@bk.ru>
2023-12-23 00:13:56 +0300

    Initial commit

---
+++ src/Task1_INNException.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+
+public class INNException extends Exception{
+    private final BigInteger innNum;
+
+    public INNException(BigInteger inn){
+        super("ИНН с номером " + inn + " не действителен");
+        innNum = inn;
+    }
+
+    public BigInteger getInnNum(){
+        return innNum;
+    }
+}
